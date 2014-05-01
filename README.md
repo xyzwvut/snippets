88e230981f665cd254b5e13bb1bdb9b1ccb2dd22
Sebastian <sb@flexpage.net>
2014-05-01 10:37:44 +0200

    Check nesting of braces in a string

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Various code snippets
 
 find_first_char.c
     Find the first non repeated character in a string
+properly_nested.c
+    Check nesting of braces in a string
---
+++ properly_nested.c
@@ -0,0 +1,67 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Return true if the input string contains
+ * a properly nested sequence of braces
+ */
+int
+properly_nested(const char *str)
+{
+    /* Remove non brace characters */
+    const char *s = str;
+    int level = 0;
+    size_t idx;
+
+    for (idx = 0; idx < strlen(str); idx++) {
+        switch (str[idx]) {
+        case '(' :
+            level++;
+            break;
+        case ')' :
+            level--;
+            if (level < 0)
+                goto exit;
+            break;
+        default:
+            break;
+        }
+    }
+exit:
+    if (level == 0) {
+        printf("All good\n");
+    } else {
+        printf("Braces error at position %zd\n", idx);
+    }
+
+    return !!level;
+}
+
+int
+main(void)
+{
+    char *p1 = "()";
+    char *p2 = "(())";
+    char *p3 = "(()())";
+    char *p4 = "(n)";
+    char *p5 = "n";
+
+    char *p6 = "(";
+    char *p7 = "())";
+    char *p8 = ")()";
+    char *p9 = "nn)()";
+
+    /* Expected passed */
+    assert(properly_nested(p1));
+    assert(properly_nested(p2));
+    assert(properly_nested(p3));
+    assert(properly_nested(p4));
+    assert(properly_nested(p5));
+
+    /* Expected failed */
+    assert(!properly_nested(p6));
+    assert(!properly_nested(p7));
+    assert(!properly_nested(p9));
+}
+
