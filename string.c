6a039d96185074a26518d803db7f0ff29519863d
Sebastian <sb@flexpage.net>
2014-05-24 15:22:33 +0200

    String handling in C

---
+++ string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void
+reverse(char s[])
+{
+    int i=0, j;
+    int end = strlen(s);
+
+	/*Take care that we go only till end-1 as array is indexed from 0 */
+    for(i=0, j=end-1; i<j; i++, j--) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
+
+
+int
+palindrome(char s[])
+{
+    int i=0, j;
+    int end = strlen(s);
+
+	for(i=0, j=end-1; i<j; i++, j--) {
+	    if (s[i] != s[j])
+            return 0;
+    }
+    return 1;
+}
