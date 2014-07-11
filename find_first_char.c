80397ec4c43fba6c8c749c96e9fc259262d43e6f
Sebastian <sb@flexpage.net>
2014-07-11 23:02:59 +0200

    Improved find_first_char
    - Fixed bugs
    - Added TODOs

--- find_first_char.c
+++ find_first_char.c
@@ -1,21 +1,40 @@
+#include <limits.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 /*
  * Find the first non-repeating character in a string ABCA -> B
+ *
+ * - Function signature?
+ * - Trade-offs in return value (char vs. error code vs. position)
+ * - What type/level of parameter validation?
+ * - Which testing input NULL, "\0", ...
+ * - UCHAR_MAX vs. 256 vs. 1 << sizeof(char) * 8
+ * - Space vs. Time trade-off?
+ * - bigOh Notation O(n)
+ * - Extensions
+ *   - Input set
+ *   - Generic?
+ *
+ * TODO:
+ * - sort + walk through?
+ * - no memory overhead O(n^2)
+ * - assert()?
  */
 
 char
 find_first_char1(const char *str)
 {
-    size_t len = strlen(str);
+    size_t len;
     int *ctrs;
 
-    if (len < 1)
-        return '\0';
+    if (str == NULL)
+      return '\0';
 
-    ctrs = malloc( sizeof(char) * len);
+    len = strlen(str);
+
+    ctrs = malloc(sizeof(int) * UCHAR_MAX);
     if (ctrs == NULL)
         return '\0';
 
@@ -32,9 +51,13 @@ find_first_char1(const char *str)
     return '\0';
 }
 
+
 char
 find_first_char2(const char *str)
 {
+    if (str == NULL)
+        return '\0';
+
     for(size_t i = 0; i < strlen(str); i++) {
         if (strchr(str, str[i]) == strrchr(str, str[i]))
             return str[i];
@@ -47,7 +70,12 @@ find_first_char2(const char *str)
 int
 main(void)
 {
-    char *str = "ABCAA";
+    char *str = NULL;
+    printf("String '%s'\n", str);
+    printf("Method 1 '%c'\n", find_first_char1(str));
+    printf("Method 2 '%c'\n", find_first_char2(str));
+
+    str = "ABCAA";
     printf("String '%s'\n", str);
     printf("Method 1 '%c'\n", find_first_char1(str));
     printf("Method 2 '%c'\n", find_first_char2(str));
